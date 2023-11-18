569e303a1ca3de7283d50684f50a9d4bb61852ee
mafalaz <lazuardi264@gmail.com>
2023-11-18 09:30:17 +0700

    Add Article

---
+++ app/src/main/java/com/bangkit/recycleme/DetailActivity.kt
@@ -0,0 +1,49 @@
+package com.bangkit.recycleme
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var tvDetailName: TextView
+    private lateinit var tvDescription: TextView
+    private lateinit var imgPhoto: ImageView
+    private lateinit var tvPrice: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        tvDetailName = findViewById(R.id.tv_detail_name)
+        tvDescription = findViewById(R.id.tv_detail_description)
+        imgPhoto = findViewById(R.id.img_item_photo)
+        tvPrice = findViewById(R.id.tv_item_price)
+
+        val receivedData = if (Build.VERSION.SDK_INT >= 33) {
+            @Suppress("DEPRECATION")
+            intent.getParcelableExtra<Phone>("DATA")
+        } else {
+            @Suppress("DEPRECATION")
+            intent.getParcelableExtra<Phone>("DATA")
+        }
+        if (receivedData != null) {
+            supportActionBar?.title = "Detail"
+            val photo = receivedData.photo
+            val text = receivedData.name
+            val deskripsi = receivedData.detailDescription
+            val price = receivedData.price
+
+            imgPhoto.setImageResource(photo)
+            tvDetailName.text = text
+            tvDescription.text = deskripsi
+            tvPrice.text = "Mulai Dari: $price"
+        }
+    }
+}
--- app/src/main/java/com/bangkit/recycleme/ui/article/ArticleFragment.kt
+++ app/src/main/java/com/bangkit/recycleme/ui/article/ArticleFragment.kt
@@ -1,42 +1,78 @@
 package com.bangkit.recycleme.ui.article
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bangkit.recycleme.DetailActivity
+import com.bangkit.recycleme.ListPhoneAdapter
+import com.bangkit.recycleme.Phone
+import com.bangkit.recycleme.R
 import com.bangkit.recycleme.databinding.FragmentArticleBinding
 
 class ArticleFragment : Fragment() {
-
-    private var _binding: FragmentArticleBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var rvPhone: RecyclerView
+    private val list = ArrayList<Phone>()
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val articleViewModel =
-            ViewModelProvider(this).get(ArticleViewModel::class.java)
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_article, container, false)
 
-        _binding = FragmentArticleBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        rvPhone = view.findViewById(R.id.rv_phone)
+        rvPhone.setHasFixedSize(true)
 
-        val textView: TextView = binding.textHome
-        articleViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        list.addAll(getListPhone())
+
+        showRecyclerList()
+
+        setHasOptionsMenu(true)
+
+        return view
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun getListPhone(): ArrayList<Phone> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val maxDescriptionLength = 25
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+        val dataPrice = resources.getStringArray(R.array.data_price)
+        val detailDescription = resources.getStringArray(R.array.data_description)
+        val listPhone = ArrayList<Phone>()
+        for (i in dataName.indices) {
+            val description = if (dataDescription[i].length > maxDescriptionLength) {
+                val truncatedDescription = dataDescription[i].substring(0, maxDescriptionLength)
+                "$truncatedDescription... Read More"
+            } else {
+                dataDescription[i]
+            }
+            val phone = Phone(dataName[i], description, detailDescription[i], dataPhoto.getResourceId(i, -1), dataPrice[i])
+            listPhone.add(phone)
+        }
+
+        return listPhone
+    }
+
+    private fun showRecyclerList() {
+        val layoutManager = LinearLayoutManager(requireContext())
+        rvPhone.layoutManager = layoutManager
+        val listPhoneAdapter = ListPhoneAdapter(list)
+        rvPhone.adapter = listPhoneAdapter
+
+        listPhoneAdapter.setOnItemClickCallback(object : ListPhoneAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Phone) {
+                val intentToDetail = Intent(requireContext(), DetailActivity::class.java)
+                intentToDetail.putExtra("DATA", data)
+                startActivity(intentToDetail)
+            }
+        })
     }
 }
