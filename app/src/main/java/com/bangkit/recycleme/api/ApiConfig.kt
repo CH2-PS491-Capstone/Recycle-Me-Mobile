28a4b95c35a0db6dcbd599d63725e9b778ae0f30
mafalaz <lazuardi264@gmail.com>
2023-12-06 11:35:29 +0700

    Add Feature Week 2 - Project Report

--- app/src/main/java/com/bangkit/recycleme/api/ApiConfig.kt
+++ app/src/main/java/com/bangkit/recycleme/api/ApiConfig.kt
@@ -22,7 +22,7 @@ class ApiConfig {
                 .addInterceptor(authInterceptor)
                 .build()
             val retrofit = Retrofit.Builder()
-                .baseUrl("http://34.128.114.200:80/")
+                .baseUrl("YOUR_API_KEY")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build()
